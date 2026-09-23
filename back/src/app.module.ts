97f11672fe6f332b165a13a14929774fdf13f3e6
VM Dane <victoiremoukoko3@gmaill.com>
2026-09-23 17:00:50 +0200

    feat(back): add ValidationPipe, GlobalExceptionFilter, ResponseInterceptor, CORS, Helmet, ThrottlerModule

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/frameworks/nest/database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'default',
+        ttl: 60000, // fenêtre de 60 secondes
+        limit: 100, // 100 requêtes max par IP sur cette fenêtre
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
